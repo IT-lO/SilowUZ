52b005bd9ba67e8e51c5da1fc813c039c8122986
AdrianZurawskiii <adrian.zurawski@auctane.com>
2026-03-29 13:11:22 +0200

    SIL-15: updated ui

--- app/src/main/java/com/itio/silowuz/screen/ExerciseScreen.kt
+++ app/src/main/java/com/itio/silowuz/screen/ExerciseScreen.kt
@@ -1,12 +1,12 @@
 package com.itio.silowuz.screen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,13 +14,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -28,9 +39,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.core.text.isDigitsOnly
 import com.itio.silowuz.R
 import kotlin.time.Duration
 
@@ -52,87 +67,55 @@ fun ExerciseScreen(paddingValues: PaddingValues){
 
     var showExerciseDialog by remember { mutableStateOf(false) }
     var seriesList by remember { mutableStateOf(listOf<ExerciseSeries>()) }
+    var reps by remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues)
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LazyColumn{
-            items(seriesList){ series ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(4.dp),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Column(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            Text(
-                                text = stringResource(
-                                id = R.string.series_info_reps,
-                                series.reps
-                            ),
-                                modifier = Modifier
-                                    .padding(16.dp)
-                            )
-                            Text(
-                                text = stringResource(
-                                id = R.string.series_info_series,
-                                series.series
-                            ),
-                                modifier = Modifier
-                                    .padding(16.dp)
-                            )
-                            Text(
-                                text = stringResource(
-                                id = R.string.series_info_weight,
-                                series.weight
-                            ),
-                                modifier = Modifier
-                                    .padding(16.dp)
-                            )
-                        }
-                        Column(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .width(IntrinsicSize.Min),
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            Button(
-                                onClick = {},
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                            ) {
-                                Text(text = "Usuń")
-                            }
-                            Button(
-                                onClick = {},
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                            ) {
-                                Text(text = "Edytuj")
-                            }
-                        }
-                    }
+        Scaffold(
+            floatingActionButton = {
 
+                FloatingActionButton(
+                    onClick = { showExerciseDialog = true },
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add Exercise")
                 }
             }
-        }
-        Spacer(Modifier.weight(1f))
-        Button(
-            modifier = Modifier
-                .fillMaxWidth(),
-
-            onClick = { showExerciseDialog = true}
-        ) {
-            Text(text = "Add new series")
+        ) { paddingValues ->
+            LazyColumn(
+                modifier = Modifier
+                    .padding(paddingValues)
+            ){
+                items(seriesList){ series ->
+                    OutlinedCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(PaddingValues(16.dp, 4.dp))
+                            .align(Alignment.CenterHorizontally),
+                        border = BorderStroke(1.dp, Color.Black)
+                    ) {
+                        Text(
+                            text = series.reps.toString(),
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
+                        Text(
+                            text = "sss",
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
+                        Text(
+                            text = "sss",
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
+                    }
+                }
+            }
         }
 
         if(showExerciseDialog){
@@ -142,9 +125,17 @@ fun ExerciseScreen(paddingValues: PaddingValues){
                     tonalElevation = 8.dp
                 ) {
                     Column(
-                        modifier = Modifier.padding(16.dp)
+                        modifier = Modifier.padding(paddingValues)
                     ) {
                         Text(text = "Informations about series")
+                        TextField(
+                            value = reps,
+                            onValueChange = { reps = it },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                            label = { Text("Reps", color = MaterialTheme.colorScheme.onSurface) },
+                            modifier = Modifier.padding(8.dp),
+
+                        )
                         Spacer(modifier = Modifier.height(16.dp))
                         Row(
                             horizontalArrangement = Arrangement.End,
@@ -154,7 +145,7 @@ fun ExerciseScreen(paddingValues: PaddingValues){
                                 Text(text = "Cancel")
                             }
                             Button(onClick = {
-                                seriesList = seriesList + ExerciseSeries(10,3,99.9)
+                                seriesList = seriesList + ExerciseSeries(Integer.getInteger(reps, 0),3,99.9)
                                 showExerciseDialog = false
                             }) {
                                 Text(text = "Save")
